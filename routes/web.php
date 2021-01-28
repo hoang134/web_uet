cd375f17d84351ac48466b8494a1866bd5d61dda
ManhHoang <vumanhhoang140799@gmail.com>
2021-01-29 01:20:18 +0700

    fix conflict

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::get('cet-infomation-chucnang','App\Http\Controllers\Home\CetInfomationCon
 Route::get('cet-notification-events','App\Http\Controllers\Home\CetNotificationController@cet_notification_events')->name('cet.notification.event');
 Route::get('cet-notification-exams','App\Http\Controllers\Home\CetNotificationController@cet_notification_exams')->name('cet.notification.exam');
 Route::get('cet-notification-exam-detail/{Makythi}','App\Http\Controllers\Home\CetNotificationController@cet_notification_exam_detail')->name('cet.notification.exam.detail');
-
+Route::get('question-detail/{id}','App\Http\Controllers\Home\HomeController@question_detail')->name('question.detail');
 //
 Route::get('home','App\Http\Controllers\Home\HomeController@index')->name('home');
 Route::get('home/question','App\Http\Controllers\Home\HomeController@question')->name('home.question');
