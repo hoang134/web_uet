ca6c531ff71b90c5251a09b0fdcddc2a246af83c
hoang134 <hoang13499.kshb@gmail.com>
2021-01-29 00:39:41 +0700

    fix service js

---
+++ public/js/admin/service/create.js
@@ -0,0 +1,140 @@
+$(function () {
+    // Button add field
+    $(document).on('click', '.add-field', function (e) {
+        let fieldList = $('.field-list')
+        let lastField = fieldList.find('.field-item').last()
+        let lastFieldIndex = lastField.data('field-index')
+        fieldList.append(fieldItemHtml(lastFieldIndex + 1))
+    })
+
+    // Button delete field
+    $(document).on('click', '.delete-field', function (e) {
+        let currentFieldList = $(this).closest('.field-list')
+        let lastField = currentFieldList.find('.field-item')
+        if (lastField.length > 1) {
+            $(this).closest('.field-item').remove()
+        }
+    })
+
+    // Listen select.field-type change
+    $(document).on('change', 'select.field-type', function (e) {
+        let currentField = $(this).closest('.field-item')
+        let fieldIndex = parseInt(currentField.data('field-index'))
+
+        let val = $(this).val()
+        if (val === 'checkbox' || val === 'radio' || val === 'select') {
+            // show value
+            currentField.find('.show-value').addClass('card').html(valueFrameHtml(fieldIndex))
+        } else {
+            // hidden value
+            currentField.find('.show-value').removeClass('card').text('')
+        }
+    })
+
+    // Button add value
+    $(document).on('click', '.add-value', function (e) {
+        let currentField = $(this).closest('.field-item')
+        let fieldIndex = parseInt(currentField.data('field-index'))
+        let currentValueFrame = $(this).closest('.show-value')
+        let lastValue = currentValueFrame.find('.value-item').last()
+        let lastValueIndex = lastValue.data('value-index')
+        currentValueFrame.find('.list-value').append(valueItemHtml(fieldIndex, lastValueIndex + 1))
+    })
+
+    // Button delete value
+    $(document).on('click', '.delete-value', function (e) {
+        let currentValueFrame = $(this).closest('.show-value')
+        let lastValue = currentValueFrame.find('.value-item')
+        if (lastValue.length > 1) {
+            $(this).closest('.value-item').remove()
+        }
+    })
+
+    var fieldItemHtml = (fieldIndex) => {
+        return `<div class="field-item card mt-3" data-field-index="${fieldIndex}">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between">
+                            <h5 class="card-title">Field</h5>
+                            <i class="delete-field fa fa-trash-alt cursor-pointer"></i>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name${fieldIndex}" class="col-sm-2 col-form-label">Name</label>
+                            <div class="col-sm-8 col-lg-6">
+                                <input type="text" class="form-control" id="name${fieldIndex}" placeholder="Name" name="fields[${fieldIndex}][name]">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="type${fieldIndex}" class="col-sm-2 col-form-label">Type</label>
+                            <div class="col-sm-8 col-lg-6">
+                                <select class="field-type form-control" id="type${fieldIndex}" name="fields[${fieldIndex}][type]">
+                                    <option value="text">Text</option>
+                                    <option value="number">Number</option>
+                                    <option value="textarea">Textarea</option>
+                                    <option value="checkbox">Checkbox</option>
+                                    <option value="radio">Radio</option>
+                                    <option value="select">Select</option>
+                                    <option value="file">File</option>
+                                    <option value="date">Date</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Validate</label>
+                            <div class="col-sm-8 col-lg-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="fields[${fieldIndex}][validate]" id="validate1${fieldIndex}" value="required">
+                                    <label class="form-check-label" for="validate1${fieldIndex}">
+                                        Required
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="fields[${fieldIndex}][validate]" id="validate2${fieldIndex}" value="option">
+                                    <label class="form-check-label" for="validate2${fieldIndex}">
+                                        Option
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="show-value"></div>
+
+                    </div>
+                </div>`
+    }
+
+    var valueFrameHtml = (fieldIndex) => {
+        return `<div class="card-body">
+                    <h6 class="card-title">Values</h6>
+                    <div class="list-value">
+                        <div class="value-item form-group row" data-value-index="0">
+                            <label for="value${fieldIndex}0" class="col-sm-2 col-form-label">Value</label>
+                            <div class="col-sm-8 col-lg-6">
+                                <input type="text" class="form-control" id="value${fieldIndex}0" placeholder="Value" name="fields[${fieldIndex}][values][]">
+                            </div>
+                            <div class="position-relative">
+                                <div class="position-absolute delete-center">
+                                    <div class="delete-value fa fa-minus-circle cursor-pointer"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <div class="add-value fa fa-plus-circle cursor-pointer"></div>
+                    </div>
+                </div>`
+    }
+
+    var valueItemHtml = (fieldIndex, valueIndex) => {
+        return `<div class="value-item form-group row" data-value-index="${valueIndex}">
+                    <label for="value${fieldIndex}${valueIndex}" class="col-sm-2 col-form-label">Value</label>
+                    <div class="col-sm-8 col-lg-6">
+                        <input type="text" class="form-control" id="value${fieldIndex}${valueIndex}" placeholder="Value" name="fields[${fieldIndex}][values][]">
+                    </div>
+                    <div class="position-relative">
+                        <div class="position-absolute delete-center">
+                            <div class="delete-value fa fa-minus-circle cursor-pointer"></div>
+                        </div>
+                    </div>
+                </div>`
+    }
+});
--- resources/views/admin/messengers/index.blade.php
+++ resources/views/admin/messengers/index.blade.php
@@ -1,57 +1,60 @@
 @extends('admin.layout')
 @section('content')
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Cập nhật thông tin trung tâm</h1>
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="">Trang chủ</a></li>
-      <li class="breadcrumb-item" aria-current="page">Quản lý tin nhắn</li>
-      <li class="breadcrumb-item" aria-current="page">Quản lý tin nhắn</li>
-    </ol>
-</div>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12 col-xl-12 chat-messenger">
-            <div class="card-messenger mb-sm-3 mb-md-0 contacts_card">
-                <div class="card-header">
-                    <div class="input-group">
-                        <input type="text" placeholder="Search..." name="" class="form-control search-messenger">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text search_btn"><i class="fas fa-search"></i></span>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Cập nhật thông tin trung tâm</h1>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="">Trang chủ</a></li>
+            <li class="breadcrumb-item" aria-current="page">Quản lý tin nhắn</li>
+            <li class="breadcrumb-item" aria-current="page">Quản lý tin nhắn</li>
+        </ol>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 col-xl-12 chat-messenger">
+                <div class="card-messenger mb-sm-3 mb-md-0 contacts_card">
+                    <div class="card-header">
+                        <div class="input-group">
+                            <input type="text" placeholder="Search..." name="" class="form-control search-messenger">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text search_btn"><i class="fas fa-search"></i></span>
+                            </div>
                         </div>
                     </div>
+                    <div class="card-body contacts_body">
+                        <ul class="contacts">
+                            @foreach($listUserTos as $user)
+                                <li class="active">
+                                    <a href="{{ route('messengers.detail', $user->user_to) }}">
+                                        <div class="d-flex bd-highlight">
+                                            <div class="img_cont">
+                                                <img src="{{asset('images/1.png')}}">
+                                                <span class="online_icon"></span>
+                                            </div>
+                                            <div class="user_info">
+                                                @php
+                                                    $messenger = DB::table('messengers')->orderBy('created_at','desc')->where('user_from', $user->user_to)->first();
+                                                @endphp
+                                                <span>{{$messenger->user_from}}</span>
+                                                <p style="overflow: hidden;white-space: nowrap;text-overflow: ellipsis;width: 30vh">{{$messenger->content}}</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <div class="card-footer"></div>
                 </div>
-                <div class="card-body contacts_body">
-                    <ul class="contacts">
-                    @foreach($messengers as $messenger)
-                        <li class="active">
-                            <a href="/admin/detail/messengers/{{$messenger->user_id_from}}">
-                                <div class="d-flex bd-highlight">
-                                    <div class="img_cont">
-                                        <img src="{{asset('images/1.png')}}">
-                                        <span class="online_icon"></span>
-                                    </div>
-                                    <div class="user_info">
-                                        <span>{{\App\Models\User::find($messenger->user_id_from)->name}}</span>
-                                        <p style="overflow: hidden;white-space: nowrap;text-overflow: ellipsis;width: 30vh">{{$messenger->content}}</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                    @endforeach
-                    </ul>
-                </div>
-                <div class="card-footer"></div>
-            </div> 
+            </div>
         </div>
     </div>
-</div>
-@endsection    
+@endsection
 @section('script')
-   <script>
+    <script>
         $(document).ready(function(){
-          $('#action_menu_btn').click(function(){
-            $('.action_menu').toggle();
-          });
+            $('#action_menu_btn').click(function(){
+                $('.action_menu').toggle();
+            });
         });
-   </script>
+    </script>
 @endsection
